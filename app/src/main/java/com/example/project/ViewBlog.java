6616f91e296ce194c4fe43a5cc6ef77b5a1387a1
TharushiNavodya2000 <it21057106@my.sliit.lk>
2022-09-14 01:40:54 +0530

    Add dependancy recycle view

---
+++ app/src/main/java/com/example/project/ViewBlog.java
@@ -0,0 +1,10 @@
+package com.example.project;
+
+public class ViewBlog
+{
+    String place[];
+    String desription[];
+    int images[]= {R.drawable.galle,R.drawable.kandy,R.drawable.mahamewnawa,R.drawable.nelumkuluna,R.drawable.seema_malakaya,
+            R.drawable.sigiriya,R.drawable.vatadageya};
+
+}
