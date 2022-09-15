89dbc2b7150610956cb5eb5ecb0ef27586432e21
TharushiNavodya2000 <it21057106@my.sliit.lk>
2022-09-15 12:42:13 +0530

    Blog Test 1

--- app/src/main/java/com/example/project/Blog.java
+++ app/src/main/java/com/example/project/Blog.java
@@ -14,7 +14,6 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-
 public class Blog extends AppCompatActivity {
 
     private EditText placeTxt;
