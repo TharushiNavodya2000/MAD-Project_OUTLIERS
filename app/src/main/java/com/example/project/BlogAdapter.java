5f93063b99f758b8ad87b8c6229f8c48036b6efb
TharushiNavodya2000 <it21057106@my.sliit.lk>
2022-09-17 14:05:22 +0530

    Add new File

---
+++ app/src/main/java/com/example/project/BlogAdapter.java
@@ -0,0 +1,55 @@
+package com.example.project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BlogAdapter extends RecyclerView.Adapter<BlogAdapter.MyViewHolder> {
+    String place[];
+    String description[];
+    int images[];
+    Context context;
+
+    public BlogAdapter(Context context,String place[],String description[],int images[])
+    {
+        this.place = place;
+        this.description = description;
+        this.images = images;
+        this.context = context;
+    }
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.blog_list,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.places.setText(place[position]);
+        holder.descriptions.setText(description[position]);
+        holder.images.setImageResource(images[position]);
+    }
+
+    public int getItemCount() {
+        return place.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView places;
+        TextView descriptions;
+        ImageView images;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            places = itemView.findViewById(R.id.place);
+            descriptions = itemView.findViewById(R.id.description);
+            images = itemView.findViewById(R.id.image);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/project/BlogView.java
@@ -0,0 +1,52 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
+
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class BlogView extends AppCompatActivity {
+
+    ImageView women;
+    ImageView heart;
+    AnimatedVectorDrawableCompat avd;
+    AnimatedVectorDrawable avd2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_view);
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        |View.SYSTEM_UI_FLAG_FULLSCREEN
+                        |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        women =findViewById(R.id.women);
+        heart = findViewById(R.id.heart);
+        final Drawable drawable = heart.getDrawable();
+        women.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                heart.setAlpha(0.70f);
+                if(drawable instanceof AnimatedVectorDrawableCompat)
+                {
+                    avd = (AnimatedVectorDrawableCompat) drawable;
+                    avd.start();
+                }
+                else if(drawable instanceof AnimatedVectorDrawable)
+                {
+                    avd2 = (AnimatedVectorDrawable) drawable;
+                    avd2.start();
+                }
+            }
+        });
+
+    }
+}
