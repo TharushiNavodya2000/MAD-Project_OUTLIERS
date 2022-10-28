373416c9b99b9a518a23efdffe8a5ca6fdbcfcd4
TharushiNavodya2000 <it21057106@my.sliit.lk>
2022-10-28 16:45:13 +0530

    Add blog change

--- app/src/main/java/com/example/project/Model.java
+++ app/src/main/java/com/example/project/Model.java
@@ -2,22 +2,53 @@ package com.example.project;
 
 public class Model
 {
-    private String imageUri;
+    private String date;
+    private String discription;
+    private String place;
+    private String time;
+
+    public Model(String date,String discription,String place,String time)
+    {
+        this.date = date;
+        this.discription = discription;
+        this.place = place;
+        this.time = time;
+    }
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
     public Model()
     {
 
     }
-    public Model(String imageUri)
-    {
-        this.imageUri = imageUri;
-    }
 
-    public String getImageUri() {
-        return imageUri;
-    }
-
-    public void setImageUri(String imageUri)
-    {
-        this.imageUri = imageUri;
-    }
 }
--- app/src/main/java/com/example/project/ViewBlog.java
+++ app/src/main/java/com/example/project/ViewBlog.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.graphics.drawable.GradientDrawable;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ViewBlog extends AppCompatActivity {
-
-
     String places[];
     String desriptions[];
     int images[]= {R.drawable.galle,R.drawable.kandy,R.drawable.mahamewnawa,R.drawable.nelumkuluna,R.drawable.seema_malakaya,
-                R.drawable.sigiriya,R.drawable.vatadageya};
+            R.drawable.sigiriya,R.drawable.vatadageya};
+    Button buttonItinerary;
+    Button buttonguide;
+    Button add;
     RecyclerView recyclerView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +25,37 @@ public class ViewBlog extends AppCompatActivity {
         places = getResources().getStringArray(R.array.place);
         desriptions = getResources().getStringArray(R.array.description);
         recyclerView = findViewById(R.id.recycleView);
+        //modify
+        add = findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //buttonItinerary = findViewById(R.id.itinerary);
+        //buttonItinerary.setOnClickListener(new View.OnClickListener() {
+        //@Override
+        //    public void onClick(View view) {
+        //        Intent intent = new Intent(ViewBlog.this, Create_Itinerary.class);
+        //        startActivity(intent);
+        //    }
+        //});
+
         BlogAdapter myAdapter = new BlogAdapter(this,places,desriptions,images);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //buttonguide = findViewById(R.id.guide);
+        //buttonguide.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        Intent intent = new Intent(ViewBlog.this, BottomNavigation.class);
+        //        Toast.makeText(ViewBlog.this,"Successful Guide",Toast.LENGTH_LONG).show();
+        //        startActivity(intent);
+        //    }
+        //});
+
+
     }
 }
