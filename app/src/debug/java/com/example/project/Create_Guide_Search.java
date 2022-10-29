8afd5f945fc2845cb3f0b7fd20665733550e98e0
IT21077692 - Kasthurirathne K. K. I <it21077692@my.sliit.lk>
2022-10-29 11:54:22 +0530

    Commit

--- app/src/debug/java/com/example/project/Create_Guide_Search.java
+++ app/src/debug/java/com/example/project/Create_Guide_Search.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class Create_Guide_Search extends AppCompatActivity {
+
+    Button CreateGuideBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_guide_search);
 
+        CreateGuideBtn = findViewById(R.id.createGuideBtn);
+        CreateGuideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Create_Guide_Search.this, GuidesForm.class);
+                startActivity(intent);
+            }
+        });
+
     }
-
-
 
 }
--- app/src/main/java/com/example/project/ViewGuidesFeed.java
+++ app/src/main/java/com/example/project/ViewGuidesFeed.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -19,13 +21,15 @@ public class ViewGuidesFeed extends AppCompatActivity {
     int images[]= {R.drawable.galle,R.drawable.kandy,R.drawable.mahamewnawa,R.drawable.nelumkuluna,R.drawable.seema_malakaya,
             R.drawable.sigiriya,R.drawable.vatadageya};
 
-    RecyclerView recyclerView;
+    Dialog myDialog;
 
-    FloatingActionButton GoToNextPage;
+    RecyclerView recyclerView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_guides_feed);
+        setContentView(R.layout.create_guide_popup);
+        myDialog = new Dialog(this);
 
         places = getResources().getStringArray(R.array.users);
         desriptions = getResources().getStringArray(R.array.guideDescription);
@@ -35,14 +39,20 @@ public class ViewGuidesFeed extends AppCompatActivity {
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        GoToNextPage = findViewById(R.id.guide);
-        GoToNextPage.setOnClickListener(new View.OnClickListener() {
+    }
+
+    public void ShowPopup(View view) {
+        TextView txtclose;
+        Button btnOk;
+        myDialog.setContentView(R.layout.create_guide_popup);
+        txtclose = (TextView) myDialog.findViewById(R.id.txtclose);
+        btnOk = (Button) myDialog.findViewById(R.id.btnok);
+        txtclose.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ViewGuidesFeed.this, Create_Guide_Search.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                myDialog.dismiss();
             }
         });
-
+        myDialog.show();
     }
 }
